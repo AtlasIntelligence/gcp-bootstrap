94011954e86a5dd9b99ec4bb5ee596c0822f38c9
Felix <felix.tepel@gmail.com>
2023-01-09 17:23:23 +0400

    revert empty subdomain

--- modules/cloudrun-loadbalancer/main.tf
+++ modules/cloudrun-loadbalancer/main.tf
@@ -65,8 +65,7 @@ resource "google_compute_region_network_endpoint_group" "serverless_neg" {
 }
 
 resource "google_dns_record_set" "record_set" {
-  # use subdomain if provided. Else use dns name.
-  name = var.service_dns_prefix != "" ? "${lower(var.service_dns_prefix)}.${data.google_dns_managed_zone.dns_zone.dns_name}" : data.google_dns_managed_zone.dns_zone.dns_name
+  name = "${lower(var.service_dns_prefix)}.${data.google_dns_managed_zone.dns_zone.dns_name}"
   type = "A"
   ttl  = 300
 
