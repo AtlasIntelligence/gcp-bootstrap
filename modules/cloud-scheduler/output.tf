947c005af8c0f1bde24e489111599d622e4efc99
Felix <felix.tepel@gmail.com>
2023-01-11 11:38:43 +0400

    feature: add cloud scheduler module

--- modules/cloud-scheduler/output.tf
+++
@@ -1,9 +0,0 @@
-# Return service URL
-output "url" {
-  value = google_cloud_run_service.this.status[0].url
-}
-
-# Return Service name
-output "name" {
-  value = google_cloud_run_service.this.name
-}
