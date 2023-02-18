56ff3ac6961dff0287cc3f4f6ce76bc1e689040e
Felix <felix.tepel@gmail.com>
2023-02-18 19:18:55 +0400

    ci: adjust record_set

--- modules/cloudrun-managed-domain/main.tf
+++ modules/cloudrun-managed-domain/main.tf
@@ -32,7 +32,7 @@ resource "google_cloud_run_domain_mapping" "default" {
   }
 }
 
-resource "google_dns_record_set" "example_cname_record" {
+resource "google_dns_record_set" "record_set" {
   name = "${local.service_dns_record}."
   type = "CNAME"
   ttl  = 300
